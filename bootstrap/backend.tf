59d5337bae6a0db7bf71d55322b02810dde1315e
JuanJDlp <juanjosedelapava1@gmail.com>
2026-03-29 22:45:28 -0500

    Deployed final application

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-  bucket = "tf-state-k8s-webapp-prod"
+  bucket = "tf-state-k8s-webapp-deployment-juanj"
   key = "backend-infra/terraform.tfstate"
   region = "us-east-1"
 }
